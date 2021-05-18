91b27fa1f04703644c56e60eda364ec08a66fae0
Michal Sara <michal.sara99@gmail.com>
2021-05-18 18:58:18 +0200

    part of initial commit

--- asm_x86/exp_funcs_.asm
+++ asm_x86/exp_funcs_.asm
@@ -104,6 +104,89 @@ exp_avx2_pd					proc uses ebx,
 							mov eax,1
 							jz done
 
+							vmovapd ymm0,ymmword ptr [ebx]					;; x = ymm0		
+							vmovupd ymm1,ymmword ptr [log2e_pd]
+							vmulpd ymm1,ymm1,ymm0							;; a = ymm1
+							vmovapd ymm2,ymm1								;; a = ymm2
+							vcmpltpd ymm2,ymm2, ymm7						;; p = ymm2
+							vmovupd ymm6,ymmword ptr [one_pd]
+							vandpd ymm2,ymm2,ymm6							;; p = ymm2
+							vsubpd ymm1,ymm1,ymm2							;; a = ymm1
+							vextractf128 xmm3,ymm1,0						;; a_l = xmm3
+							vextractf128 xmm4,ymm1,1						;; a_h = xmm4	
+							;; this could be macro:
+							vsubpd xmm3,xmm3 ,xmmword ptr [opfournine_pd]
+							vaddpd xmm3,xmm3,xmmword ptr [long_real]	
+							vpsubq xmm3,xmm3,xmmword ptr [long_real]
+							vsubpd xmm4,xmm4,xmmword ptr [opfournine_pd]
+							vaddpd xmm4,xmm4,xmmword ptr [long_real]				;; k_l = xmm3
+							vpsubq xmm4,xmm4,xmmword ptr [long_real]				;; k_h = xmm4
+							
+							vroundpd ymm1,ymm1,00000001b					;; p = ymm1
+
+							vmovupd ymm5,ymmword ptr [c1_pd]
+							vmovupd ymm6,ymmword ptr [c2_pd]
+							vmulpd ymm2,ymm1,ymm5							;; a = ymm2
+							vsubpd ymm0,ymm0,ymm2							;; x = ymm0
+							vmulpd ymm2,ymm1,ymm6							;; a = ymm2
+							vsubpd ymm0,ymm0,ymm2							;; x = ymm0
+							
+							vmovupd ymm5,ymmword ptr [m_ecoef_pd]
+							vmovupd ymm6,ymmword ptr [m_ecoef_pd + 32]
+							vmulpd ymm2,ymm0,ymm5
+							vaddpd ymm2,ymm2,ymm6							;; a = ymm2
+
+							vmovupd ymm5,ymmword ptr [m_ecoef_pd + 64]
+							vmovupd ymm6,ymmword ptr [m_ecoef_pd + 96]
+							vfmadd213pd ymm2,ymm0,ymm5	
+							vfmadd213pd ymm2,ymm0,ymm6						;; a = ymm2
+
+							vmovupd ymm5,ymmword ptr [m_ecoef_pd + 128]
+							vmovupd ymm6,ymmword ptr [m_ecoef_pd + 160]
+							vfmadd213pd ymm2,ymm0,ymm5	
+							vfmadd213pd ymm2,ymm0,ymm6						;; a = ymm2
+
+							vmovupd ymm5,ymmword ptr [m_ecoef_pd + 192]
+							vmovupd ymm6,ymmword ptr [m_ecoef_pd + 224]
+							vfmadd213pd ymm2,ymm0,ymm5	
+							vfmadd213pd ymm2,ymm0,ymm6						;; a = ymm2
+
+							vmovupd ymm5,ymmword ptr [m_ecoef_pd + 256]
+							vmovupd ymm6,ymmword ptr [m_ecoef_pd + 288]
+							vfmadd213pd ymm2,ymm0,ymm5	
+							vfmadd213pd ymm2,ymm0,ymm6						;; a = ymm2
+
+							vmovupd ymm5,ymmword ptr [m_ecoef_pd + 320]
+							vmovupd ymm6,ymmword ptr [m_ecoef_pd + 352]
+							vfmadd213pd ymm2,ymm0,ymm5	
+							vfmadd213pd ymm2,ymm0,ymm6						;; a = ymm2
+
+							;; p = 2^k
+							vpaddq xmm3,xmm3, xmmword ptr [int_tentwothree] 
+							vpaddq xmm4,xmm4, xmmword ptr [int_tentwothree]
+							vinsertf128 ymm3,ymm3,xmm4,1
+							vpsllq ymm3,ymm3,52								;; k = ymm3
+							vmulpd ymm3,ymm3,ymm2
+
+							cmp ecx,1
+							je short one_left
+							cmp ecx,2
+							je short two_left
+							cmp ecx,3
+							je short three_left
+
+			one_left:		vmovsd real8 ptr [edx],xmm3   
+							jmp short done
+			two_left:		vmovsd real8 ptr [edx],xmm3
+							movhlps xmm2,xmm3
+							vmovsd real8 ptr [edx + 8],xmm2 
+							jmp short done
+			three_left:		vextractf128 xmm6,ymm3,1	
+							movhlps xmm2,xmm3
+							vmovsd real8 ptr [edx],xmm3
+							vmovsd real8 ptr [edx + 8],xmm2
+							vmovsd real8 ptr [edx + 16],xmm6
+
 				done:		ret
 exp_avx2_pd					endp
 
@@ -177,6 +260,68 @@ exp_avx2_ps					proc uses ebx,
 			too_short:		or ecx,ecx								
 							mov eax,1
 							jz done
+
+							vmovaps ymm0,ymmword ptr [ebx]					;; x = ymm0		
+							vminps ymm0,ymm0,ymmword ptr [exp_hi_ps]
+							vmaxps ymm0,ymm0,ymmword ptr [exp_lo_ps]
+							vmovups ymm1,ymmword ptr [log2e_ps]
+							vmulps ymm1,ymm1,ymm0							
+							vaddps ymm1,ymm1,ymmword ptr [zero_point_five]  ;; fx = ymm1
+							vmovaps ymm2,ymm1								;; tmp = ymm2
+							vroundps ymm2,ymm2,00000001b					;; tmp = ymm2
+							vcmpgtps ymm7,ymm2,ymm1							;; mask = ymm7
+							vandps ymm7,ymm7,ymmword ptr [one_ps]
+							vsubps ymm1,ymm2,ymm7
+							vmulps ymm2,ymm1,ymmword ptr [c1_ps]			;; tmp = ymm2
+							vmulps ymm3,ymm1,ymmword ptr [c2_ps]			;; z = ymm3
+							vsubps ymm0,ymm0,ymm2							;; x = ymm0
+							vsubps ymm0,ymm0,ymm3							;; x = ymm0
+							vmulps ymm3,ymm0,ymm0							;; z = ymm3
+
+							vmovups ymm4,ymmword ptr [m_ecoef_ps]			;; y = ymm4
+							vfmadd213ps ymm4,ymm0,ymmword ptr [m_ecoef_ps + 32]
+							vfmadd213ps ymm4,ymm0,ymmword ptr [m_ecoef_ps + 64]
+							vfmadd213ps ymm4,ymm0,ymmword ptr [m_ecoef_ps + 96]
+							vfmadd213ps ymm4,ymm0,ymmword ptr [m_ecoef_ps + 128]
+							vfmadd213ps ymm4,ymm0,ymmword ptr [m_ecoef_ps + 160]
+							vfmadd213ps ymm4,ymm3,ymm0
+							vaddps ymm4,ymm4,ymmword ptr [one_ps]
+							
+							;; p = 2^k
+							vcvttps2dq ymm5,ymm1
+							vpaddd ymm5,ymm5,ymmword ptr [int_onetwoseven]
+							vpslld ymm5,ymm5,23 							
+							vmulps ymm5,ymm5,ymm4
+							
+							movaps xmm6,xmm5	
+							cmp ecx,4
+							jl short rem_left
+							vextractf128 xmm6,ymm5,1 
+							movaps xmmword ptr [edx],xmm5
+							add edx,16
+							sub ecx,4
+							jz done
+
+
+			rem_left:		cmp ecx,1
+							je short one_left
+							cmp ecx,2
+							je short two_left
+							cmp ecx,3
+							je short three_left
+
+			one_left:		movss real4 ptr [edx],xmm6
+							jmp short done
+			two_left:		insertps xmm2,xmm6,01000000b
+							movss real4 ptr [edx],xmm6
+							movss real4 ptr [edx + 4],xmm2	
+							jmp short done
+			three_left:		insertps xmm2,xmm6,01000000b
+							insertps xmm4,xmm6,10000000b
+							movss real4 ptr [edx],xmm6
+							movss real4 ptr [edx + 4],xmm2
+							movss real4 ptr [edx + 8],xmm4
+
 
 				done:		ret
 exp_avx2_ps					endp
--- asm_x86/log_funcs_.asm
+++ asm_x86/log_funcs_.asm
@@ -73,10 +73,61 @@ log_avx2_pd					proc uses ebx,
 							mov eax,1
 							jz done
 
+							vmovapd ymm0,ymmword ptr[ebx]
+							vpand ymm6,ymm0,ymmword ptr[qint_twentyfourseven] 
+							vpsrlq ymm6,ymm6,52									;; exps64 = ymm6
+							vmovdqu ymm5,ymmword ptr[int_sixfourtwozero]		;;  gTo32bitExp = ymm5
+							vpermd ymm6,ymm5,ymm6 								;; exps32_sse = ymm6
+							vmovapd xmm6,xmm6
+							psubd  xmm6,xmmword ptr[int_tentwothree]			;;  normExps = ymm6
+							vcvtdq2pd ymm6,xmm6									;; expsPD = ymm6
+							vmovupd	ymm7,ymmword ptr[qint_twentyfourseven]
+							vandnpd	ymm7,ymm7,ymm0
+							vorpd ymm7,ymm7,ymmword ptr [qint_tentwothree]		;; y = ymm7						
+							vsubpd ymm1,ymm7,ymmword ptr[one_pd]
+							vaddpd ymm2,ymm7,ymmword ptr[one_pd]
+							vdivpd ymm1,ymm1,ymm2								;; t = ymm1
+							vmulpd ymm2,ymm1,ymm1								;; t2 = ymm2
+							vmulpd ymm3,ymm1,ymm2								;; t3 = ymm3
+
+							vmovupd ymm4,ymmword ptr[m_lcoef_pd]
+							vfmadd213pd ymm4,ymm3,ymm1							;; term01 = ymm4
+							vmulpd ymm1,ymm2,ymm3								;; t5 = ymm1
+
+							vmovupd ymm5,ymmword ptr[m_lcoef_pd + 32]
+							vfmadd213pd ymm5,ymm1,ymm4							;; terms012 = ymm5
+							vmulpd ymm3,ymm1,ymm2								;; t7 = ymm3
+
+							vmovupd ymm4,ymmword ptr[m_lcoef_pd + 64]
+							vfmadd213pd ymm4,ymm3,ymm5							;; terms0123 = ymm4
+							vmulpd ymm3,ymm3,ymm2								;; t9 = ymm3
+
+							vmovupd ymm5,ymmword ptr[m_lcoef_pd + 96]
+							vfmadd213pd ymm5,ymm3,ymm4							;; terms01234 = ymm5
+							vmulpd ymm3,ymm5,ymmword ptr [sqrthf_pd]			;; log2_y = ymm3
+							vaddpd ymm3,ymm3,ymm6								;; log2_x = ymm3
+							vdivpd ymm3,ymm3,ymmword ptr[log_q_pd]
+
+							cmp ecx,1
+							je short one_left
+							cmp ecx,2
+							je short two_left
+							cmp ecx,3
+							je short three_left
+
+			one_left:		vmovsd real8 ptr [edx],xmm3   
+							jmp short done
+			two_left:		vmovsd real8 ptr [edx],xmm3
+							movhlps xmm2,xmm3
+							vmovsd real8 ptr [edx + 8],xmm2 
+							jmp short done
+			three_left:		vextractf128 xmm6,ymm3,1	
+							movhlps xmm2,xmm3
+							vmovsd real8 ptr [edx],xmm3
+							vmovsd real8 ptr [edx + 8],xmm2
+							vmovsd real8 ptr [edx + 16],xmm6
+
 			done:			ret
-
-
-							ret
 log_avx2_pd					endp
 
 ;;		extern "C" bool log_avx2_ps(float const* x, int n, float* out);
@@ -153,6 +204,73 @@ log_avx2_ps					proc uses ebx,
 			too_short:		or ecx,ecx								
 							mov eax,1
 							jz done
+
+							vxorpd ymm7,ymm7,ymm7
+							vmovapd ymm0,ymmword ptr [ebx]					; x = ymm0
+							vcmpleps ymm1,ymm0,ymm7							; invalid_mask = ymm1
+							vmaxps ymm0,ymm0,ymmword ptr [int_min_norm]	
+							vpsrld ymm2, ymm0,23							; imm0 = ymm2
+							vmovups ymm7,ymmword ptr [int_mantisa]
+							vandnps ymm0,ymm7,ymm0	
+							vorps ymm0,ymm0,ymmword ptr [one_half_ps]
+							vpsubd ymm2,ymm2,ymmword ptr [int_onetwoseven]
+							vcvtdq2ps ymm3,ymm2								; e = ymm3
+							vaddps ymm3,ymm3,ymmword ptr [one_ps]
+							vcmpltps ymm4,ymm0,ymmword ptr [sqrthf_ps]		; mask = ymm4
+							vandps ymm5,ymm0,ymm4							; tmp = ymm5
+							vsubps ymm0,ymm0,ymmword ptr [one_ps]
+							vandps ymm6,ymm4,ymmword ptr [one_ps]
+							vsubps ymm3,ymm3,ymm6
+							vaddps ymm0,ymm0,ymm5
+							vmulps ymm6,ymm0,ymm0							; z = ymm6
+							vmovups ymm7,ymmword ptr [m_lcoef_ps]			; y = ymm7
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 32]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 64]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 96]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 128]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 160]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 192]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 224]
+							vfmadd213ps ymm7,ymm0,ymmword ptr [m_lcoef_ps + 256]
+							vmulps ymm7,ymm7,ymm0
+							vmulps ymm7,ymm7,ymm6 
+							vmulps ymm5,ymm3,ymmword ptr [log_q1_ps]
+							vaddps ymm7,ymm7,ymm5
+							vmulps ymm5,ymm6,ymmword ptr [one_half_ps]
+							vsubps ymm7,ymm7,ymm5
+							vmulps ymm5,ymm3,ymmword ptr [log_q2_ps]
+							vaddps ymm0,ymm0,ymm7
+							vaddps ymm0,ymm0,ymm5
+							vorps ymm0,ymm0,ymm1
+
+							movaps xmm6,xmm0	
+							cmp ecx,4
+							jl short rem_left
+							vextractf128 xmm6,ymm0,1 
+							movaps xmmword ptr [edx],xmm0
+							add edx,16
+							sub ecx,4
+							jz done
+
+
+			rem_left:		cmp ecx,1
+							je short one_left
+							cmp ecx,2
+							je short two_left
+							cmp ecx,3
+							je short three_left
+
+			one_left:		movss real4 ptr [edx],xmm6
+							jmp short done
+			two_left:		insertps xmm2,xmm6,01000000b
+							movss real4 ptr [edx],xmm6
+							movss real4 ptr [edx + 4],xmm2	
+							jmp short done
+			three_left:		insertps xmm2,xmm6,01000000b
+							insertps xmm4,xmm6,10000000b
+							movss real4 ptr [edx],xmm6
+							movss real4 ptr [edx + 4],xmm2
+							movss real4 ptr [edx + 8],xmm4
 
 			done:			ret
 log_avx2_ps					endp
