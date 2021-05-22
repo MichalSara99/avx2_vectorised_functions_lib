9ee52ba336550f207338007f10ad6f09b12235a2
Michal Sara <michal.sara99@gmail.com>
2021-05-22 15:10:28 +0200

    CHANGEs: 1) added hyperbolic tangens (both SPFP,DPFP)

--- headers/hyp_funcs.h
+++ headers/hyp_funcs.h
@@ -16,6 +16,12 @@ extern "C" bool sinh_avx2_pd(double const *in_aligned_32, int size, double *out_
 // packed single-precision floating-point hyperbolic sine
 extern "C" bool sinh_avx2_ps(float const *in_aligned_32, int size, float *out_aligned_32);
 
+// packed double-precision floating-point hyperbolic tangens
+extern "C" bool tanh_avx2_pd(double const *in_aligned_32, int size, double *out_aligned_32);
+
+// packed single-precision floating-point hyperbolic tangens
+extern "C" bool tanh_avx2_ps(float const *in_aligned_32, int size, float *out_aligned_32);
+
 } // namespace __packed_avx2_
 
 namespace avx2_math
@@ -71,6 +77,32 @@ bool sinh_avx2_packed(double const *in_aligned_32, int size, double *out_aligned
 bool sinh_avx2_packed(float const *in_aligned_32, int size, float *out_aligned_32)
 {
     return __packed_avx2_::sinh_avx2_ps(in_aligned_32, size, out_aligned_32);
+}
+
+/**
+ * Packed double-precision floating-point hyperbolic tangens
+ *
+ * \param in_aligned_32
+ * \param size
+ * \param out_aligned_32
+ * \return boolean indicating success or failure
+ */
+bool tanh_avx2_packed(double const *in_aligned_32, int size, double *out_aligned_32)
+{
+    return __packed_avx2_::tanh_avx2_pd(in_aligned_32, size, out_aligned_32);
+}
+
+/**
+ * Packed single-precision floating-point hyperbolic tangens
+ *
+ * \param in_aligned_32
+ * \param size
+ * \param out_aligned_32
+ * \return boolean indicating success or failure
+ */
+bool tanh_avx2_packed(float const *in_aligned_32, int size, float *out_aligned_32)
+{
+    return __packed_avx2_::tanh_avx2_ps(in_aligned_32, size, out_aligned_32);
 }
 
 } // namespace avx2_math
